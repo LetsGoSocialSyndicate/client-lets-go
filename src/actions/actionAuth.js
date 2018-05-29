007b748938213045ac49bb10584e43ebd9361d95
slledru <slledru@msn.com>
2018-05-29 15:21:54 -0600

    added API call for login

--- src/actions/actionAuth.js
+++ src/actions/actionAuth.js
@@ -4,8 +4,19 @@
 import { LOGIN_SUCCESS, LOGIN_FAILED, LOGOUT,
   SIGNUP_SUCCESS, SIGNUP_FAILED  } from '../constants'
 
-const loginSubmit = () => {
-  return (dispatch) => {
+const loginSubmit = (username, password) => {
+  const url = `${process.env.REACT_APP_API_URL}/login`
+
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+  return async (dispatch) => {
+    const response = await fetch('/login')
     dispatch({ type: LOGIN_SUCCESS })
   }
 }
