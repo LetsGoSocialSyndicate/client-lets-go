228d68cf68884c211b3a8be92c1c4232d41fa98a
slledru <slledru@msn.com>
2018-05-29 15:22:39 -0600

    login form implemented

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,63 @@
 /*
  * Copyright 2018, Socializing Syndicate Corp.
  */
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import '../assets/styles/Login.css'
 
 import { loginSubmit } from '../actions/actionAuth'
 
-const LoginForm = (props) => {
-  return (
-    <div className="page">
-      Login Form
-      <form className="form-group">
-      </form>
-    </div>
-  )
+class LoginForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      token: ''
+    }
+  }
+
+  onusernameChange = (event) => {
+    event.preventDefault()
+    this.setState({ ...this.state, username: event.target.value })
+  }
+
+  onPasswordChange = (event) => {
+    event.preventDefault()
+    this.setState({ ...this.state, password: event.target.value })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.loginSubmit({ username: this.state.username, password: this.state.password })
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <form className="login-form container" onSubmit={this.onSubmit}>
+          <div className="row form-group">
+            <label className="col-form-label">Username:</label>
+            <input className="form-control"
+              placeholder='Username'
+              onChange={this.onusernameChange}
+              value={this.state.username} required />
+          </div>
+          <div className="row form-group">
+            <label className="col-form-label">Password:</label>
+            <input className="form-control"
+              type="password"
+              placeholder='Password'
+              onChange={this.onPasswordChange}
+              value={this.state.password} required />
+          </div>
+          <input className="row btn btn-md submit" type="submit"/>
+          <p>{this.props.error}</p>
+        </form>
+      </div>
+    )
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
