05c1e390cc57aa2177f2bb12d46ee0574aa64fb6
Christian Bourlier <chrisbourlier@hotmail.com>
2018-06-04 17:00:46 -0600

    styling on events

--- src/components/EventFeeds.js
+++ src/components/EventFeeds.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 import Groot from '../assets/images/groot.png'
 import bottom_line from '../assets/images/bottom_line.png'
 import requestButton from '../assets/images/Test.png'
+import circle from '../assets/images/bgCircleGradient.png'
 
 class EventFeeds extends Component {
   // componentDidMount(){
@@ -33,7 +34,47 @@ class EventFeeds extends Component {
       duration_min: 60,
       description: "We'll be pedalling around town looking for networking opportunities",
       owner: "Eric"
-    }
+    },
+    {
+      id: 3,
+      title: 'Hanging with Groot',
+      location: 'Boulder, CO',
+      icon_name: 'tree',
+      start_time: 'Today at 11:55am',
+      duration_min: 120,
+      description: "We're getting together to bash some bad guys and play video games in the park",
+      owner: "Groot"
+    },
+    {
+      id: 4,
+      title: 'Bike Riding with Eric',
+      location: 'Boulder, CO',
+      icon_name: 'bicycle',
+      start_time: 'Tomorrow at 5:00pm',
+      duration_min: 60,
+      description: "We'll be pedalling around town looking for networking opportunities",
+      owner: "Eric"
+    },
+    {
+      id: 5,
+      title: 'Hanging with Groot',
+      location: 'Boulder, CO',
+      icon_name: 'tree',
+      start_time: 'Today at 11:55am',
+      duration_min: 120,
+      description: "We're getting together to bash some bad guys and play video games in the park",
+      owner: "Groot"
+    },
+    {
+      id: 6,
+      title: 'Bike Riding with Eric',
+      location: 'Boulder, CO',
+      icon_name: 'bicycle',
+      start_time: 'Tomorrow at 5:00pm',
+      duration_min: 60,
+      description: "We'll be pedalling around town looking for networking opportunities",
+      owner: "Eric"
+    },
   ]
 
 
@@ -44,11 +85,13 @@ class EventFeeds extends Component {
           <li className="list-group-item event-li" key={event.id}>
             <div className="row row-event-owner">
               <div>
+                <img className="owner-image-bg"
+                  src={circle} />
                 <img className="owner-image"
                   src={Groot} alt="I am Groot!"/>
               </div>
               <div className="event-snapshot">
-                {event.owner}<br />
+                <h4>{event.owner}</h4>
                 {event.start_time}<br />
                 {event.location}
               </div>
@@ -56,12 +99,14 @@ class EventFeeds extends Component {
             <div className="row row-event-info">
               <h3>{event.title}</h3><br />
               {event.icon_name}<br />
+              <i class="far fa-tree"></i>
               <Link className="request-to-join"
                 to={`events/${event.id}`}>
                 <img className="requestButton" src={requestButton} />
               </Link>
             </div>
             <img className="eventDivider" src={bottom_line} />
+            <br />
           </li>
         )
       })
