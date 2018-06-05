e465dd8d5a1beb0757a002f225913c62b7df5b46
slledru <slledru@msn.com>
2018-06-05 16:46:21 -0600

    display owner image

--- src/components/Event/NewEvent.js
+++ src/components/Event/NewEvent.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { addNewEvent } from '../../actions/actionFeeds'
+import circle from '../../assets/images/bgCircleGradient.png'
+import '../../assets/styles/Events.css'
 
 const NewEvent = (props) => {
   return (
-    <div>Create a new event</div>
+    <div className="container-events-top">
+      <div className="container container-events">
+        <div className="row row-event-owner">
+          <div>
+            <img className="owner-image-bg" src={circle} alt="background image"/>
+            <img className="owner-image" src={require('../../assets/images/bastian.jpg')} alt="waaa bee boo dada!"/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
