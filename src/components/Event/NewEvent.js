f8f3920ef6ba6dbe0edc756ec4c8c4f6cdbfdd58
slledru <slledru@msn.com>
2018-06-05 18:42:01 -0600

    preparing for new event form

--- src/components/Event/NewEvent.js
+++ src/components/Event/NewEvent.js
@@ -2,8 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { addNewEvent } from '../../actions/actionFeeds'
-import circle from '../../assets/images/bgCircleGradient.png'
+import { Field, reduxForm } from 'redux-form'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import 'react-datepicker/dist/react-datepicker.css'
 import '../../assets/styles/Events.css'
+import { DATE_FORMAT } from '../../constants'
+
+export const TITLE_FIELD = "title"
+export const LOCATION_FIELD = "location"
+export const CATEGORY_FIELD = "category"
+export const DESCRIPTION_FIELD = "description"
+export const START_TIME_FIELD = "startTime"
+export const END_TIME_FIELD = "endTime"
+export const ICON_URL_FIELD = "iconUrl"
 
 const NewEvent = (props) => {
   return (
@@ -11,8 +23,6 @@ const NewEvent = (props) => {
       <div className="container container-events">
         <div className="row row-event-owner">
           <div>
-            <img className="owner-image-bg" src={circle} alt="background image"/>
-            <img className="owner-image" src={require('../../assets/images/bastian.jpg')} alt="waaa bee boo dada!"/>
           </div>
         </div>
       </div>
