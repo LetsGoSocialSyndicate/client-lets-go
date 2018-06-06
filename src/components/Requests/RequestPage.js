98f208b6dba1832df1572edfa341761f43336f38
slledru <slledru@msn.com>
2018-06-06 12:00:03 -0600

    my events page

--- src/components/Requests/RequestPage.js
+++ src/components/Requests/RequestPage.js
@@ -5,6 +5,11 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import bottom_line from '../../assets/images/bottom_line.png'
+import circle from '../../assets/images/bgCircleGradient.png'
+import acceptedButton from '../../assets/images/Test.png'
+import messageButton from '../../assets/images/Test.png'
+import '../../assets/styles/Events.css'
 import '../../assets/styles/Requests.css'
 import { fetchMyEventFeeds } from '../../actions/actionFeeds'
 
@@ -13,19 +18,79 @@ class RequestPage extends Component {
     this.props.fetchMyEventFeeds(this.props.user, false)
   }
 
+  renderEvents() {
+    return (
+      Object.values(this.props.eventFeeds)
+      .map(event => {
+        let eventDate = {
+          date: new Date(event.event_start_time).toDateString().substr(4,7),
+          time: (event.event_start_time).substr(11,5)
+        }
+        return (
+          <li className="list-group-item event-li" key={event.event_id}>
+            <div className="row row-event-owner">
+              <div>
+                <img className="owner-image-bg"
+                  src={circle} />
+                <img className="owner-image"
+                  src={event.user_image_url} alt="waaa bee boo dada!"/>
+              </div>
+              <div className="event-snapshot">
+                <h4>{event.first_name} {event.last_name}<br />
+                  <small>
+                    {eventDate.date} at {eventDate.time}<br />
+                    {event.event_location}
+                  </small>
+                </h4>
+              </div>
+            </div>
+            <div className="row">
+              <div className="event-image-holder event-image-overlap">
+                <img className="event-image-bg"
+                  src={circle} />
+                <img className="event-image"
+                  src={event.event_icon_url}
+                  alt={event.event_title} /><br />
+              </div>
+              <div className="buttons-overlap">
+                <Link className="request-to-join"
+                  to={`events/${event.event_id}`}>
+                  <div className="oval screenButton">accepted
+                  </div>
+                </Link>
+                <Link className="request-to-join"
+                  to={`events/${event.event_id}`}>
+                  <div className="oval screenButton">message
+                  </div>
+                </Link>
+              </div>
+            </div>
+            <img className="eventDivider" src={bottom_line} />
+            <br />
+          </li>
+        )
+      })
+    )
+  }
+
   render() {
-    if (!this.props.eventFeeds.isLoaded) {
+    if (!this.props.isLoaded) {
       return ( <div>Loading...</div>)
     }
     return (
       <div className="container-requests-top">
-      my events
+        <div className="container container-events">
+          <ul className="list-group">
+            {this.renderEvents()}
+          </ul>
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log('mapStateToProps', state)
   return { ...state.eventFeeds, ...state.user }
 }
 
