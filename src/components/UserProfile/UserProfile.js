1d2af0529497d32bcd072f9e4c50f9f9e258a363
Tanya <mellene@gmail.com>
2018-06-06 12:03:47 -0600

    profile page under construction

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,58 +1,43 @@
-/*
- * Copyright 2018, Socializing Syndicate Corp.
- */
+/* Copyright 2018, Socializing Syndicate Corp. */
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 // import {withRouter} from "react-router-dom"
 import {bindActionCreators} from 'redux'
 import '../../assets/styles/UserProfile.css'
-import whiteCircle from '../../assets/images/whiteCircle.png'
 import {fetchUser} from '../../actions/actionUser'
 
-//Need to pass id or user's email here from navbar button onClick.
-//User is logged in, we just need his id or email
 class UserProfile extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchUser(this.props.auth.email)
   }
   render() {
-    console.log("UserProfile render and props are:", this.props)
-    // if (!this.props.?????) {
-    //   return <div>Loading...</div>
-    // }
+    console.log("UserProfile render and props are:", this.props.user.user)
+    if (!this.props.user) {
+      return <div className="page">Loading...</div>
+    }
     return (
-      <div className="page">
-        Hello World! It is User Profile page.
-        {/* <li className="list-group-item user-li" key={user.id}>
-          <div className="row row-user">
-            <div>
-              <div className="white-circle">
-                {whiteCircle}
-              </div>
-              <img className="owner-image"
-                src={require('../assets/images/bastian.jpg')} alt="waaa bee boo dada!"/>
-            </div>
-            <div className="user-name">
-              <h4>{user.name}</h4>
-            </div>
+      <div className="container-user-top">
+        <div className="container container-user">
+          <div className="image">
+            <img className="user-image" src={require('../../assets/images/bastian.jpg')} alt="waaa bee boo dada!"/>
+          </div>
+          <div className="user-name">
+            <h3>{this.props.user.user.first_name} {this.props.user.user.last_name}</h3>
           </div>
           <div className="row row-user-about">
-            <h3>{user.about}</h3>
+            <h3>{this.props.user.user.about}</h3>
           </div>
           <div>
             <h4>Add photos...</h4>
           </div>
-          <br />
-        </li> */}
-      </div>)
-}
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    auth: state.auth
-  }
+  return {user: state.user, auth: state.auth}
 }
 
 const dispatchToProps = (dispatch) => bindActionCreators({
