f3021b88c80b4d236e35b458a266eb93ce1ead47
slledru <slledru@msn.com>
2018-06-09 15:28:05 -0600

    code cleanup

--- src/components/EventFeeds.js
+++ src/components/EventFeeds.js
@@ -21,53 +21,44 @@ class EventFeeds extends Component {
   renderEvents() {
     return (
       Object.values(this.props.eventFeeds)
-      .map(event => {
-        let eventDate = {
-          date: new Date(event.event_start_time).toDateString().substr(4,7),
-          time: (event.event_start_time).substr(11,5)
-        }
-        return (
-          <li className="list-group-item event-li" key={event.event_id}>
-            <div className="row row-event-owner">
-              <div>
-                <Link to={`/profile/${event.user_id}`}>
-                <img className="owner-image-bg"
-                  src={circle} />
-                <img className="owner-image"
-                  src={event.user_image_url} alt="host picture"/>
-                </Link>
+        .map(event => {
+          let eventDate = {
+            date: new Date(event.event_start_time).toDateString().substr(4,7),
+            time: (event.event_start_time).substr(11,5)
+          }
+          return (
+            <li className="list-group-item event-li" key={event.event_id}>
+              <div className="row row-event-owner">
+                <div>
+                  <Link to={`/profile/${event.user_id}`}>
+                  <img className="owner-image-bg" src={circle} />
+                  <img className="owner-image" src={event.user_image_url} alt="host picture"/>
+                  </Link>
+                </div>
+                <div className="event-snapshot">
+                  <h4>{event.first_name} {event.last_name}<br />
+                  <small>
+                  {eventDate.date} at {eventDate.time}<br />
+                  {event.event_location}
+                </small></h4>
+                </div>
               </div>
-              <div className="event-snapshot">
-                <h4>{event.first_name} {event.last_name}<br />
-                <small>
-                {eventDate.date} at {eventDate.time}<br />
-                {event.event_location}
-              </small></h4>
-              </div>
-            </div>
-            <div className="row row-event-info">
-              <h3>{event.event_title}</h3>
-              <div className="event-image-holder">
-                <img className="event-image-bg"
-                  src={circle} />
-                <img className="event-image"
-                  src={event.event_icon_url}
-                  alt="event icon" /><br />
-              </div>
-              {/* <Link className="request-to-join"
-                to={`events/request/${event.event_id}`}> */}
+              <div className="row row-event-info">
+                <h3>{event.event_title}</h3>
+                <div className="event-image-holder">
+                  <img className="event-image-bg" src={circle} />
+                  <img className="event-image" src={event.event_icon_url} alt="event icon" /><br />
+                </div>
                 <div className="oval"
                   onClick={this.props.handleRequest.bind(null, event.event_id, this.props.user.id, this.props.token)}>
-                  <img className="requestButton"
-                    src={requestButton} />
+                  <img className="requestButton" src={requestButton} />
                 </div>
-              {/* </Link> */}
-            </div>
-            <img className="eventDivider" src={bottom_line} />
-            <br />
-          </li>
-        )
-      })
+              </div>
+              <img className="eventDivider" src={bottom_line} />
+              <br />
+            </li>
+          )
+        })
     )
   }
 
